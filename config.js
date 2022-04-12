9721502066ca9af6aa0d061866afb886cbe83058
pengengantibaju <pengengantibaju@gmail.com>
2022-04-13 02:42:44 +0700

    ya

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  Token: "",  // write your selfbot discord account token
-  Prefix: "!", // customizer
-  BotOwner: "", // write the user id of your own discord account.
+  Token: "NTcwNTg5NDc3OTIwMzA5MjU5.YlXSfA.L0TSfNDX8py6bXVsSMbFs2O6clQ",  // write your selfbot discord account token
+  Prefix: "woi", // customizer
+  BotOwner: "570589477920309259", // write the user id of your own discord account.
   OwoBotID: "408785106942164992", // Default Do Not Change!
-  UserID: ["",""], // Sometimes he will tag other users so that the account is not banned from the owo bot, write different discord account ids here.
-  Writes: ["Test Message :)","Spam Message","Hello World!","Hi Guys :)","You Are Noob!"], // In order to prevent the account from being banned from the owo bot, it sends random messages from time to time, in this section there are random messages it will send.
-  GuildID: "" // Type id of the discord server where the selfbot will run.
+  UserID: ["376278784757858304","962268162269790248"], // Sometimes he will tag other users so that the account is not banned from the owo bot, write different discord account ids here.
+  Writes: ["Test Message :)","we Message","Hello World!","Hi Guys :)","You Are cooll!"], // In order to prevent the account from being banned from the owo bot, it sends random messages from time to time, in this section there are random messages it will send.
+  GuildID: "958091704160899154" // Type id of the discord server where the selfbot will run.
 };
